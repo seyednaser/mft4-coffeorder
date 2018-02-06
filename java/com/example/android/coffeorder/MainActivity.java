94faaa875bf51350b79ecb06d9c8d31fe4ff8ce8
seyednaser <35557032+seyednaser@users.noreply.github.com>
2018-02-06 13:17:08 +0330

    Frist commit on MainActivity

---
+++ java/com/example/android/coffeorder/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.coffeorder;
+
+//import android.app.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements CoffeListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            CoffeDetailFragment details = new CoffeDetailFragment();
+            details.setCoffe(id);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_container, details);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra("coffe", id);
+            startActivity(intent);
+        }
+    }
+}
